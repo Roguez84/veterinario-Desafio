07b256e6341855dd0109a49d50a5110b9b2d325e
Roguez84 <t.roguez984@gmail.com>
2021-04-22 21:53:40 -0400

    finished part 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#Pets
+
+# 10.times do
+#     Pet.create(
+#         name: Faker::Creature::Dog.name, 
+#         breed: Faker::Creature::Dog.breed,
+#         date_of_birth: Faker::Date.birthday(min_age:1, max_age:16)
+#     )
+# end
+
+
+#Pet Histories
+# t.string "height"
+# t.string "weight"
+# t.datetime "vet_visit_date"
+# t.datetime "created_at", null: false
+# t.datetime "updated_at", null: false
+# t.integer "pet_id"
+
+
+
+Pet.pluck(:id).each do |id|
+    3.times do
+        PetHistory.create(
+            height: rand(20..90),
+            weight: rand(10..50),
+            vet_visit_date: Faker::Date.between(from: 2.years.ago, to: Date.yesterday),
+            pet_id: id
+        )
+    end
+end
+    
